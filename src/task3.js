0208eb4501a82ac4b7fbeb84ed58f1c05013fd39
Diana <dianagomez151019@gmail.com>
2022-03-04 10:08:57 -0500

    task 3 completed

--- src/task3.js
+++ src/task3.js
@@ -1,27 +1,32 @@
 function extractValuesForKey(object, value) {
-    console.log(object.constructor.name)
-    if(object.constructor !== Object) {
-        throw new TypeError('extractValuesForKey() can only operate on object with Object as constructor');
-    }
-    let resultMap = new Map();
-    let newPath = '';
-    var path = [];
+  if (object.constructor !== Object) {
+    throw new TypeError(
+      "extractValuesForKey() can only operate on object with Object as constructor"
+    );
+  }
+  let resultMap = new Map();
+  let newPath = "";
+  var path = [];
 
-    function search(myObject) {
-        for (var key in myObject) {
-            path.push(key);
-            if(key === value) {
-                if(path.length > 1) newPath = path.join('/');
-                resultMap.set(newPath, myObject[key])
-                path = [];
-            }
-            if(myObject[key].constructor === Object) {
-                search(myObject[key]);
-            }
+  function search(myObject) {
+    for (var key in myObject) {
+      path.push(key);
+      if (key === value) {
+        if (path.length > 1) {
+          path.pop();
+          newPath = path.join("/");
         }
+        resultMap.set(newPath, myObject[key]);
+        path = [];
+      }
+      if (myObject[key].constructor === Object) {
+        search(myObject[key]);
+      }
+      path.pop();
     }
-    search(object);
-    return resultMap;
+  }
+  search(object);
+  return resultMap;
 }
 
 const myObject = {
@@ -39,5 +44,25 @@ const myObject = {
     },
   },
 };
-const result1 = extractValuesForKey(myObject, 'uuid');
+const result1 = extractValuesForKey(myObject, "uuid");
 console.log(result1);
+
+const myObject2 = {
+  uuid: 1,
+  innerOne: {
+    someKey: "some text",
+  },
+  innerTwo: {
+    uuid: 2,
+    innerThree: {
+      someOtherKey: "some other text",
+      innerFour: {
+        fourOther: {
+          uuid: 3,
+        },
+      },
+    },
+  },
+};
+const result2 = extractValuesForKey(myObject2, "uuid");
+console.log(result2);
